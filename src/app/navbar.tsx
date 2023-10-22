4f45c79ea96d4c7e2ce562e117b02b4d89e39e00
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2023-10-22 17:53:45 +0700

    feat(pract)! : Link & Navigation

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,20 +1,51 @@
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
-    <nav className="flex bg-gray-800 py-2 px-5">
+    <nav className="flex bg-gray-800 py-2 px-5 justify-between">
       <h1 className="">Navbar</h1>
-      <ul className="flex ml-5">
-        <Link href="/" className="hover:text-red-400">
-          <li className="mr-3 cursor-pointer">Home</li>
-        </Link>
-        <Link href="/about">
-          <li className="mr-3 cursor-pointer">about</li>
-        </Link>
-        <Link href="/about/profile">
-          <li className="mr-3 cursor-pointer">profile</li>
-        </Link>
-      </ul>
+      <div className="flex">
+        <ul className="flex ml-5">
+          <Link href="/">
+            <li
+              className={`mr-3 ${
+                pathname === "/" ? "text-blue-600" : ""
+              } cursor-pointer`}
+            >
+              Home
+            </li>
+          </Link>
+          <Link href="/about">
+            <li
+              className={`mr-3 ${
+                pathname === "/about" ? "text-blue-600" : ""
+              } cursor-pointer`}
+            >
+              about
+            </li>
+          </Link>
+          <Link href="/about/profile">
+            <li
+              className={`mr-3 ${
+                pathname === "/about/profile" ? "text-blue-600" : ""
+              } cursor-pointer`}
+            >
+              profile
+            </li>
+          </Link>
+        </ul>
+      </div>
+      <div>
+        <button
+          className="bg-white text-black rounded-md px-3 text-sm h-7 cursor-pointer"
+          onClick={() => router.push("/login")}
+        >
+          Login
+        </button>
+      </div>
     </nav>
   );
 }
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -4,8 +4,8 @@ export default function Template({ children }: { children: React.ReactNode }) {
   const [state, setstate] = useState(0);
   return (
     <div>
-      <h1>Template {state}</h1>
-      <button onClick={() => setstate(state + 1)}>Template Klik</button>
+      {/* <h1>Template {state}</h1>
+      <button onClick={() => setstate(state + 1)}>Template Klik</button> */}
       {children}
     </div>
   );
