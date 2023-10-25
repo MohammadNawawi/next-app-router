a30b4d233ce500349022ad2f35a58999677b4275
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2023-10-25 10:22:21 +0700

    feat(prac)! : Data Fetching

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -5,11 +5,15 @@ const data = [
     id: 1,
     name: "Mouse",
     price: 140000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/fe3e6a3f-14a3-42f7-8754-39a95bf2b2c0/zion-3-pf-basketball-shoes-vTjpz4.png",
   },
   {
     id: 2,
     name: "Keyboard",
     price: 240000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/fe3e6a3f-14a3-42f7-8754-39a95bf2b2c0/zion-3-pf-basketball-shoes-vTjpz4.png",
   },
 ];
 
--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -1,16 +1,62 @@
-type DetailProductPage = {
+type ProductPage = {
   params: {
     slug: string[];
   };
 };
 
-export default function DetailProductPage(props: DetailProductPage) {
+async function getData() {
+  // const res = await fetch("https://fakestoreapi.com/products");
+  const res = await fetch("http://localhost:3000/api/product");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch API");
+  }
+
+  return res.json();
+}
+
+export default async function ProductPage(props: ProductPage) {
   const { params } = props;
+  const products = await getData();
   console.log(params);
+  console.log(products);
 
   return (
-    <div>
-      <h1>{params.slug ? "DetailProductPage" : "Productpage"}</h1>
+    <div className="grid grid-cols-5 mt-5 place-items-center">
+      {/* <h1>{params.slug ? "DetailProductPage" : "Productpage"}</h1> */}
+      {products.data.length > 0 &&
+        products.data.map((product: any) => (
+          <div
+            key={product.key}
+            className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5 mx-5"
+          >
+            <a href="#">
+              <img
+                className="p-8 rounded-t-lg object-cover h-96 w-full"
+                src={product.image}
+                alt="product image"
+              />
+            </a>
+            <div className="px-5 pb-5">
+              <a href="#">
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">
+                  {product.title}
+                </h5>
+              </a>
+              <div className="flex items-center justify-between mt-3">
+                <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                  ${product.price}
+                </span>
+                <a
+                  href="#"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Add to cart
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
       {params.slug && (
         <>
           <h2>Category : {params.slug[0]}</h2>
